2c184f2cf0cd856c16651e2315e5bfd0d40ebef8
dorayakikun <github@dorayakikun.com>
2021-08-02 22:44:00 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use image::{imageops::FilterType::Triangle};
+
+pub fn calculate(path: &str) -> String {
+    // TODO error の実装
+    let img = image::open(path).expect("open an image file");
+
+    let smaller_img = img.resize_exact(9, 8, Triangle);
+    let grayscale_img = smaller_img.grayscale();
+    let pixels = grayscale_img.into_bytes();
+
+    let mut diff: Vec<bool> = vec![];
+    for row in 0..8 {
+        let row_start_index = row * 9;
+        for colum in 0..8 {
+            let left_pixel_index = row_start_index + colum;
+            diff.push(pixels[left_pixel_index] > pixels[left_pixel_index + 1]); 
+        }
+    }
+
+    let mut decimal_value = 0;
+    let mut hash_string = "".to_owned();
+    for (i, value) in diff.iter().enumerate() {
+        if *value {
+            decimal_value += 2 ^ (i % 8);
+        }
+        if i % 8 == 7 {
+            hash_string = format!("{}{:0>2x}", hash_string, decimal_value);
+            decimal_value = 0;
+        }
+    }
+
+    format!("0x{}", hash_string)
+}
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::{env, io::{BufWriter, Write, stdout}};
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+
+    if args.len() < 2 {
+        // TODO error の実装
+        println!("enter a file name");
+        return;
+    }
+
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    writeln!(out, "{}", dhash::calculate(&args[1])).unwrap();
+}
